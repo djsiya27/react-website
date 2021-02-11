bfbfd466a04dd74260bfd7830efadde03bf57b11
Siyabonga Majola <siyabongamavundla@yahoo.com>
2021-02-11 19:58:15 +0200

    Final Commit

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,36 +1,50 @@
-import React, {useState} from 'react'
-import Video from '../../videos/video.mp4';
-import { Button } from '../ButtonElement';
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElements";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 const HeroSection = () => {
-    const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState(false);
 
-    const onHover = () => {
-        setHover(!hover)
-    }
+  const onHover = () => {
+    setHover(!hover);
+  };
 
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1></HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive R250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-            </HeroBg>
-            <HeroContent>
-                <HeroH1></HeroH1>
-                    <HeroP>
-                        Sign up for a new account today and receive R250 in credit towards your next payment.
-                    </HeroP>
-                    <HeroBtnWrapper>
-                        <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}
-                        primary='true'
-                        dark='true'>
-                            Get Started {hover ? <ArrowForward /> : <ArrowRight /> }
-                        </Button>
-                    </HeroBtnWrapper>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
-import React, { useState } from 'react'
-import HeroSection from '../components/HeroSection';
-import Navbar from '../components/NavBar'
-import Sidebar from '../components/Sidebar'
+import React, { useState } from "react";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from "../components/InfoSection/Data";
+import Navbar from "../components/NavBar";
+import Services from "../components/Services";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <>
-            <Sidebar isOpen={isOpen} toggle={toggle} />
-            <Navbar toggle={ toggle } />   
-            <HeroSection />         
-        </>
-    )
-}
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+};
 
-export default Home
+export default Home;
